f987a7ade33815a9601d53c3dbfd31798293d892
imtiyazkth <imtiyazkth94@gmail.com>
2026-09-04 19:29:56 +0300

    your message

---
+++ app/build/generated/source/buildConfig/debug/com/personalai/os/BuildConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.personalai.os;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.personalai.os";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1.0-scaffold";
+  // Field from default config.
+  public static final String GEMINI_API_KEY = "";
+  // Field from default config.
+  public static final String TELEGRAM_BOT_TOKEN = "";
+  // Field from default config.
+  public static final String WHATSAPP_CLOUD_API_TOKEN = "";
+  // Field from default config.
+  public static final String WHATSAPP_PHONE_NUMBER_ID = "";
+}
